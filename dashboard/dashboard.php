ecfebe647da930d041c37cd36f783dac709829f6
Cris Mateo Uriarte <39321306+criskhun@users.noreply.github.com>
2023-08-23 13:15:02 +0800

    Update dashboard.php

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -23,51 +23,60 @@
 <div class="row">
   <div class="col-sm-8">
     <div class="row">
+
         <div class="col">
             <div class="custom-container3 p-5 my-2 border d-flex flex-column align-items-start">
                 <div class="d-flex justify-content-between align-items-center w-100">
                     <h5 class="m-0">Total Transaction</h5>
                     <i class="fa-solid fa-wand-magic-sparkles icon ml-2"></i>    
                 </div>
-                
-                <span>Number of transactions</span>
+                <span><h2>Number</h2></span>
             </div>
         </div>
+
         <div class="col">
-            <div class="custom-container3 p-5 my-2 border">
-                <div class="d-flex align-items-center">
-                    <h5>Number of Users</h5>
+            <div class="custom-container3 p-5 my-2 border d-flex flex-column align-items-start">
+                <div class="d-flex justify-content-between align-items-center w-100">
+                    <h5 class="m-0">Number of Users</h5>
+                    <i class="fa-solid fa-users icon ml-2"></i>    
                 </div>
-                <i class="fa-solid fa-users"></i>
+                <span><h2>Number</h2></span>
             </div>
         </div>
+
         <div class="col">
-            <div class="custom-container3 p-5 my-2 border">
-                <div class="d-flex align-items-center">
-                    <h5>Daily Transaction</h5>
+            <div class="custom-container3 p-5 my-2 border d-flex flex-column align-items-start">
+                <div class="d-flex justify-content-between align-items-center w-100">
+                    <h5 class="m-0">Daily Transaction</h5>
+                    <i class="fa-solid fa-calendar-day icon ml-2"></i>    
                 </div>
-                <i class="fa-solid fa-calendar-day"></i>
+                <span><h2>Number</h2></span>
             </div>
         </div>
-    </div>
+
     <div class="row">
+
         <div class="col">
-            <div class="custom-container3 p-5 my-2 border">
-                <div class="d-flex align-items-center">
-                    <h5>Weekly</h5>
+            <div class="custom-container3 p-5 my-2 border d-flex flex-column align-items-start">
+                <div class="d-flex justify-content-between align-items-center w-100">
+                    <h5 class="m-0">Weekly</h5>
+                    <i class="fa-solid fa-calendar-week icon ml-2"></i>    
                 </div>
-                <i class="fa-solid fa-calendar-week"></i>
+                <span><h2>Number</h2></span>
             </div>
         </div>
+
         <div class="col">
-            <div class="custom-container3 p-5 my-2 border">
-                <div class="d-flex align-items-center">
-                    <h5>Monthly</h5>
+            <div class="custom-container3 p-5 my-2 border d-flex flex-column align-items-start">
+                <div class="d-flex justify-content-between align-items-center w-100">
+                    <h5 class="m-0">Monthly</h5>
+                    <i class="fa-solid fa-calendar-days icon ml-2"></i>    
                 </div>
-                <i class="fa-solid fa-calendar-days"></i>
+                <span><h2>Number</h2></span>
+            </div>
         </div>
     </div>
-  </div>
+    
   </div>
   <div class="col-sm-4">
     <div class="custom-container3 p-5 my-2 border">
