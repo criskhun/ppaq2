b4dd4d566e09cc94b1b52808ef677a7b56d113d7
Cris Mateo Uriarte <39321306+criskhun@users.noreply.github.com>
2023-08-23 11:22:46 +0800

    Update dashboard.php

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -24,8 +24,8 @@
   <div class="col-sm-8">
     <div class="row">
         <div class="col">
-            <div class="custom-container3 p-5 my-2 border d-flex flex-column align-items-center">
-                <div class="d-flex align-items-center mb-3">
+            <div class="custom-container3 p-5 my-2 border d-flex flex-column align-items-start">
+                <div class="d-flex justify-content-between align-items-center w-100">
                     <h5 class="m-0">Total Transaction</h5>
                     <i class="fa-solid fa-wand-magic-sparkles icon ml-2"></i>    
                 </div>
