2a4f54188d10c1a70508787c4524aa008268eb28
Cris Mateo Uriarte <39321306+criskhun@users.noreply.github.com>
2023-08-24 09:37:27 +0800

    ag

---
+++ LogReg/agreeModal.php
@@ -0,0 +1,16 @@
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title" id="myModalLabel">Modal Title</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+            </div>
+            <div class="modal-body">
+                Modal content goes here...
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
