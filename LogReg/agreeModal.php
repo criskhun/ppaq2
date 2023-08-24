8151090786b6f326e7e659122b351c908374365e
Cris Mateo Uriarte <39321306+criskhun@users.noreply.github.com>
2023-08-24 09:57:34 +0800

    Update agreeModal.php

--- LogReg/agreeModal.php
+++ LogReg/agreeModal.php
@@ -2,11 +2,75 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h4 class="modal-title" id="myModalLabel">Modal Title</h4>
+                <h4 class="modal-title" id="myModalLabel">Terms and Condition</h4>
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
             </div>
             <div class="modal-body">
-                Modal content goes here...
+                <h4>Privacy Notice</h4>
+                <br>
+                <p>This Privacy Notice is adopted in compliance with Republic Act No. 10173 or the Data 
+                    Privacy Act of 2012 (DPA), its Implementing Rules and Regulations, and other relevant 
+                    policies, including issuances of the National Privacy Commission. The Philippine Ports 
+                    Authority (PPA) respects and values your data privacy rights, and makes sure that all 
+                    personal data collected from you are processed in adherence to the general principles 
+                    of transparency, legitimate purpose, and proportionality.</p>
+                <br>
+                <p>PPA collects the basic contact information of employees, clients and customers, 
+                    including their full name, address, email address, contact number, together with the 
+                    type of business and/or service that they would like to engage in including personal 
+                    identification number, when necessary. Whenever applicable, PPA will collect such 
+                    information through our Port Customer Registration System (PCRS) and other related 
+                    computerized application.</p>
+                <br>
+                <p>Personal data collected shall be used by PPA for documentation purposes, for billing and collection 
+                    of payment as a result of your availed port services or as a result of business engagement with PPA, 
+                    as an employee of PPA and whenever necessary, for government auditing purposes.</p>
+                <br>
+                <p>PPA will ensure that personal data under its custody are protected against any accidental or unlawful 
+                    destruction, alteration and disclosure as well as against any other unlawful processing. PPA will 
+                    implement appropriate security measures in storing collected personal information, depending on the 
+                    nature of the information. Personal data under the custody of the company shall be disclosed only 
+                    pursuant to a lawful purpose and to authorized recipients of such data.</p>
+                <br>
+                <br>
+                <h4>User Registration, Password and Security</h4>
+                <br>
+                <p>Upon successful completion of Registration, user ID and password will be registered. You are responsible 
+                    for maintaining the confidentiality of your username and password and you are held accountable for all 
+                    activities that occur under your account. You agree to (a) immediately notify PPA of any unauthorized 
+                    use of your user account or any other breach of security, and (b) ensure that you log out from your 
+                    account at the end of each session. PPA cannot and will not be liable for any loss or damage arising 
+                    from your failure to comply with the foregoing.</p>
+                <br>
+                <br>
+                <h4>User Conduct</h4>
+                <br>
+                <p>You agree that all information, data, text, software, photographs, graphics, messages or other materials 
+                    whether publicly posted or privately transmitted, are the sole responsibility of the person from which 
+                    such Content originated. This means that you, are entirely responsible for all Content that you upload, 
+                    post, e-mail or otherwise transmit via the PPA online system.</p>
+                <br>
+                <br>
+                <h4>Modification of Terms of Agreement</h4>
+                <br>
+                <p>PPA reserves the right to add or to change/modify the terms of this Agreement. Changes could be made by 
+                    us after the first posting to the Site and you will be deemed to have accepted any change if you continue 
+                    to access the Site after that time. This Site reserves the right to modify, suspend/cancel or discontinue 
+                    any or all channels, or service at any time without notice, make modifications and alterations in any or 
+                    all of the content, products and services contained on the site without prior notice, alterations in any 
+                    or all of the content, products and services contained on the site without prior notice.</p>
+                <br>
+                <br>
+                <h4>Security</h4>
+                <br>
+                <p>The Site has security measures in place to protect against the loss, misuse and alteration of information 
+                    under our control.</p>
+                <br>
+                <br>
+                <h4>Goverment Law</h4>
+                <br>
+                <p>PPA reserves the right to initiate any legal action against users violating all the above-mentioned terms 
+                    of the CSMS agreement.</p>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
