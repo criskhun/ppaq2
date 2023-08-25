c282b7ff65ccef9a527c08fad5ddf151a92c9f98
Cris Mateo Uriarte <39321306+criskhun@users.noreply.github.com>
2023-08-25 16:12:43 +0800

    Update script.js

--- codeGen/script.js
+++ codeGen/script.js
@@ -22,3 +22,12 @@ qrInput.addEventListener("keyup", () => {
         preValue = "";
     }
 });
+
+document.getElementById("generateButton").addEventListener("click", function () {
+    var port = document.getElementById("port").value;
+    var codeS = document.getElementById("codeS").value;
+    var year = document.getElementById("year").value;
+    
+    var generatedCode = port + "-" + codeS + "-" + year;
+    document.getElementById("codeHere").value = generatedCode;
+});
