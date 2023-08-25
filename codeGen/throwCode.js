4ab2b3178174ce0cfbf18779478e94b0914b6107
Cris Mateo Uriarte <39321306+criskhun@users.noreply.github.com>
2023-08-25 16:06:04 +0800

    Update throwCode.js

--- codeGen/throwCode.js
+++ codeGen/throwCode.js
@@ -1,14 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Get references to the elements
-    const generateButton = document.querySelector('.btn.btn-primary');
-    const codeHereInput = document.getElementById('codeHere');
-
-    // Add an event listener to the "Generate" button
-    generateButton.addEventListener('click', function() {
-        // Create the desired text
-        const generatedText = 'SSG - filename - 2023'; // You can modify this as needed
-
-        // Set the generated text as the value of the input field
-        codeHereInput.value = generatedText;
-    });
+document.getElementById("generateButton").addEventListener("click", function () {
+    var port = document.getElementById("port").value;
+    var codeS = document.getElementById("codeS").value;
+    var year = document.getElementById("year").value;
+    
+    var generatedCode = port + "-" + codeS + "-" + year;
+    document.getElementById("codeHere").value = generatedCode;
 });
