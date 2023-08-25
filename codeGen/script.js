d234290c76a8d3c631616ea50f55e61761c4b26d
Cris Mateo Uriarte <39321306+criskhun@users.noreply.github.com>
2023-08-25 16:14:44 +0800

    Update script.js

--- codeGen/script.js
+++ codeGen/script.js
@@ -1,33 +1,34 @@
 const wrapper = document.querySelector(".wrapper"),
-qrInput = wrapper.querySelector(".form input"),
-generateBtn = wrapper.querySelector(".form button"),
-qrImg = wrapper.querySelector(".qr-code img");
-let preValue;
-
-generateBtn.addEventListener("click", () => {
-    let qrValue = qrInput.value.trim();
-    if(!qrValue || preValue === qrValue) return;
-    preValue = qrValue;
-    generateBtn.innerText = "Generating QR Code...";
-    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}`;
-    qrImg.addEventListener("load", () => {
-        wrapper.classList.add("active");
-        generateBtn.innerText = "Generate QR Code";
-    });
-});
-
-qrInput.addEventListener("keyup", () => {
-    if(!qrInput.value.trim()) {
-        wrapper.classList.remove("active");
-        preValue = "";
-    }
-});
-
-document.getElementById("generateButton").addEventListener("click", function () {
-    var port = document.getElementById("port").value;
-    var codeS = document.getElementById("codeS").value;
-    var year = document.getElementById("year").value;
+          qrInput = wrapper.querySelector(".form input"),
+          generateBtn = wrapper.querySelector(".form button"),
+          qrImg = wrapper.querySelector(".qr-code img");
     
-    var generatedCode = port + "-" + codeS + "-" + year;
-    document.getElementById("codeHere").value = generatedCode;
-});
+    let preValue;
+
+    generateBtn.addEventListener("click", () => {
+        var port = document.getElementById("port").value;
+        var codeS = document.getElementById("codeS").value;
+        var year = document.getElementById("year").value;
+    
+        var generatedCode = port + "-" + codeS + "-" + year;
+        document.getElementById("codeHere").value = generatedCode;
+
+        let qrValue = generatedCode;
+        if (!qrValue || preValue === qrValue) return;
+        
+        preValue = qrValue;
+        generateBtn.innerText = "Generating QR Code...";
+        qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}`;
+        
+        qrImg.addEventListener("load", () => {
+            wrapper.classList.add("active");
+            generateBtn.innerText = "Generate QR Code";
+        });
+    });
+
+    qrInput.addEventListener("keyup", () => {
+        if (!qrInput.value.trim()) {
+            wrapper.classList.remove("active");
+            preValue = "";
+        }
+    });
