3445e880fbac23d8c79a3520755c37fc6017bfa8
Cris Mateo Uriarte <39321306+criskhun@users.noreply.github.com>
2023-08-25 10:33:31 +0800

    Update codeGen.php

--- codeGen/codeGen.php
+++ codeGen/codeGen.php
@@ -15,7 +15,29 @@
         $des = $_SESSION["designation"];
     }
 
+    if ($conn) {
+        $sql = "SELECT COUNT(*) AS codeSeries FROM codeSeriesCG_tbl";
+        $result = $conn->query($sql);
     
+        if ($result) {
+            $row = $result->fetch_assoc();
+            $codeSeries = $row["codeSeries"];
+    
+            if ($codeSeries < 1) {
+                $codeSeries = 1;
+            } else {
+                $codeSeries++;
+            }
+    
+            // Use $codeSeries for further processing or database insertion
+        } else {
+            echo "Error fetching data: " . $conn->error;
+        }
+    
+        $conn->close(); // Close the database connection
+    } else {
+        echo "Database connection failed: " . mysqli_connect_error();
+    }
     
 
     $year = date("Y");
