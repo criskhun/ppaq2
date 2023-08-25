fcd98a25f15d8d53615acff52a289d071f18152c
Cris Mateo Uriarte <39321306+criskhun@users.noreply.github.com>
2023-08-25 09:57:43 +0800

    Update headerCG.php

--- codeGen/headerCG.php
+++ codeGen/headerCG.php
@@ -50,7 +50,7 @@ session_start();
   background: #3498DB;
 }
 .wrapper{
-  height: 305px;
+  height: 325px;
   max-width: 410px;
   background: #fff;
   border-radius: 7px;
@@ -59,7 +59,7 @@ session_start();
   box-shadow: 0 10px 30px rgba(0,0,0,0.1);
 }
 .wrapper.active{
-  height: 570px;
+  height: 590px;
 }
 header h1{
   font-size: 21px;
