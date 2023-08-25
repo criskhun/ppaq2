4dabebdcf527582d3101db9a7ffa37f8686d4a9e
Cris Mateo Uriarte <39321306+criskhun@users.noreply.github.com>
2023-08-25 16:09:26 +0800

    Update throwCode.js

--- codeGen/throwCode.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("generateButton").addEventListener("click", function () {
-    var port = document.getElementById("port").value;
-    var codeS = document.getElementById("codeS").value;
-    var year = document.getElementById("year").value;
-    
-    var generatedCode = port + "-" + codeS + "-" + year;
-    document.getElementById("codeHere").value = generatedCode;
-});
