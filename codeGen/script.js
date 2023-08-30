d98e2c55b602ba03e65d04fbe3adbc91a6077d5c
Cris Mateo Uriarte <39321306+criskhun@users.noreply.github.com>
2023-08-30 14:31:17 +0800

    Update script.js

--- codeGen/script.js
+++ codeGen/script.js
@@ -7,18 +7,13 @@ let preValue;
 
 generateBtn.addEventListener("click", () => {
     let qrValue = qrInput.value.trim();
-    if (!qrValue || preValue === qrValue) return;
+    if(!qrValue || preValue === qrValue) return;
     preValue = qrValue;
     generateBtn.innerText = "Generating QR Code...";
     qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}`;
     qrImg.addEventListener("load", () => {
         wrapper.classList.add("active");
         generateBtn.innerText = "Generate QR Code";
-
-        // Show the download link
-        const downloadLink = document.getElementById("downloadLink");
-        downloadLink.href = qrImg.src;
-        downloadLink.style.display = "block";
     });
 });
 
