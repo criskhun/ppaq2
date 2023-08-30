68b4604917f51ef225279a355daee640ce8bbe6f
Cris Mateo Uriarte <39321306+criskhun@users.noreply.github.com>
2023-08-30 13:45:15 +0800

    Update script.js

--- codeGen/script.js
+++ codeGen/script.js
@@ -15,13 +15,20 @@ generateBtn.addEventListener("click", () => {
         wrapper.classList.add("active");
         generateBtn.innerText = "Generate QR Code";
 
-        // Automatically download the QR code image
-        const link = document.createElement("a");
-        link.href = qrImg.src;
-        link.download = "qr-code.png";
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
+        // Convert the image to a Blob
+        const canvas = document.createElement("canvas");
+        const context = canvas.getContext("2d");
+        canvas.width = qrImg.naturalWidth;
+        canvas.height = qrImg.naturalHeight;
+        context.drawImage(qrImg, 0, 0);
+        canvas.toBlob(blob => {
+            // Create a download link for the Blob
+            const link = document.createElement("a");
+            link.href = URL.createObjectURL(blob);
+            link.download = "qr-code.png";
+            link.click();
+            URL.revokeObjectURL(link.href); // Clean up
+        });
     });
 });
 
