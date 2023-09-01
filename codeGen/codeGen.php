26bdbd03ac7859234029522f52bfe14f6dc0ce1d
Cris Mateo Uriarte <39321306+criskhun@users.noreply.github.com>
2023-09-01 13:05:17 +0800

    adas

--- codeGen/codeGen.php
+++ codeGen/codeGen.php
@@ -53,12 +53,39 @@ ini_set('display_errors', 1);
     $year = date("Y");
 ?>
 
+<?php
+if(isset($_POST['submit'])) {
+    $docCode = 'SSG-'. $formattedCodeSeries ."-". $year;
+    $docTitle = $_POST['title'];
+    $sender = $_POST['sender'];
+    $docType = $_POST['doctype'];
+    $urgent = $_POST['urgentlvl'];
+    $date = $_POST['reminder'];
+    $comment = $_POST['comment'];
 
+    $fname = rand(1000,1000). "-" .$_FILES["file"]["name"];
+    $tname = $_FILES["file"]["tmp_name"];
+    $uploads_dir = '../codes';
+    move_uploaded_file($tname, $uploads_dir.'/'.$fname);
+
+    $sql = "INSERT INTO documentCG_tbl (docCode, title, sender, doctype, urgent, docdate, comment, docfile) VALUES ('$docCode', '$docTitle', '$sender', '$docType', '$urgent', '$date', '$comment', '$fname')";
+
+    if(mysqli_query($conn,$sql)){
+        
+        header('Location: codeGen.php');
+    exit;
+    }
+    else {
+        echo "Error";
+    }
+
+}
+?>
 
 <div class="container justify-content-center align-item-center custom-container mt-3"> 
     <div class="row" id="rowrow">
         <div class="col">
-        <form method="post" enctype="multipart/form-data" action="process_form.php">
+        <form method="post" enctype="multipart/form-data" action="">
             <div hidden>
             <input type="text" class="form-control" id="codeS" name="codeS" value="<?php echo $formattedCodeSeries; ?>">
             <input type="text" class="form-control" id="port" name="port" value="SSG">
