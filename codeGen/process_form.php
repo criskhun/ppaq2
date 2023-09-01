952a566325ff70d7460b14a0cd4ce529de64e7aa
Cris Mateo Uriarte <39321306+criskhun@users.noreply.github.com>
2023-09-01 11:39:41 +0800

    adas

---
+++ codeGen/process_form.php
@@ -0,0 +1,26 @@
+<?php
+if(isset($_POST['submit'])) {
+    $docCode = 'SSG-'. $formattedCodeSeries ."-". $year;
+    $docTitle = $_POST['title'];
+    $sender = $_POST['sender'];
+    $docType = $_POST['doctype'];
+    $urgent = $_POST['urgentlvl'];
+    $date = $_POST['reminder'];
+    $comment = $_POST['comment'];
+
+    $fname = rand(1000,1000). "-" .$_FILES["file"]["name"];
+    $tname = $_FILES["file"]["tmp_name"];
+    $uploads_dir = '../codes';
+    move_uploaded_file($tname, $uploads_dir.'/'.$fname);
+
+    $sql = "INSERT INTO documentCG_tbl (docCode, title, sender, doctype, urgent, docdate, comment, docfile) VALUES ('$docCode', '$docTitle', '$sender', '$docType', '$urgent', '$date', '$comment', '$fname')";
+
+    if(mysqli_query($conn,$sql)){
+        echo "File Successfully uploaded";
+    }
+    else {
+        echo "Error";
+    }
+
+}
+?>
