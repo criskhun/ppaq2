d608c5d43847d50ca20aff72ac44b85ff8aada22
Cris Mateo Uriarte <39321306+criskhun@users.noreply.github.com>
2023-09-01 14:33:41 +0800

    Update headerCG.php

--- codeGen/headerCG.php
+++ codeGen/headerCG.php
@@ -11,7 +11,7 @@ include("../database.php");
     <link rel="icon" href="../images/fav.png" type="image/x-icon">
     <link rel="shortcut icon" href="../images/fav.png" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js"></script>
+
     <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
