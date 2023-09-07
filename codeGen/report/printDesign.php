cf0f5792bf7c4acd8af0b6ad0dcee134770e1ca3
Cris Mateo Uriarte <39321306+criskhun@users.noreply.github.com>
2023-09-07 13:36:25 +0800

    das

--- codeGen/report/printDesign.php
+++ codeGen/report/printDesign.php
@@ -7,6 +7,21 @@
     <link rel="stylesheet" href="print.css">
 </head>
 <body>
-    <div class="page" size="A4"> </div>
+    <div class="page" size="A4">
+        <div class="top-section">
+            <div class="address">
+                <div class="address-content">
+                    <h2>Philippine Ports Authority - PMO SOCSARGEN</h2>
+                    <p>Sarangani, Region XII, PH</p>
+                </div>
+            </div>
+            <div class="contact">
+                <div class="contact-content">
+                    <div class="email"><span class="span">pmosocasrgen@ppa.com.ph</span></div>
+                    <div class="number"><span class="span">5522-6656</span></div>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
