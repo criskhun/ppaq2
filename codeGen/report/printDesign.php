c8624f43a9b25a30be258dd0396734c5cb3b81cd
Cris Mateo Uriarte <39321306+criskhun@users.noreply.github.com>
2023-09-07 13:25:19 +0800

    Update printDesign.php

---
+++ codeGen/report/printDesign.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Code Generator</title>
+    <link rel="stylesheet" href="print.css">
+</head>
+<body>
+    <div class="page" size="A4"> </div>
+</body>
+</html>
