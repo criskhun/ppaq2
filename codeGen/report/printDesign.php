3398e2aa2223e30e4cb1f8e9012a17750902aeeb
Cris Mateo Uriarte <39321306+criskhun@users.noreply.github.com>
2023-09-07 16:04:32 +0800

    adasd

--- codeGen/report/printDesign.php
+++ codeGen/report/printDesign.php
@@ -12,13 +12,14 @@
             <div class="address">
                 <div class="address-content">
                     <h2>PPA - PMO SOCSARGEN</h2>
-                    <p>Sarangani, Region XII, PH</p>
+                    <p>Makar Wharf, Labangal, General Santos City, 9500 Philippines</p>
                 </div>
             </div>
             <div class="contact">
                 <div class="contact-content">
                     <div class="email"> Email: <span class="span">pmosocasrgen@ppa.com.ph</span></div>
-                    <div class="number"> Number: <span class="span">5522-6656</span></div>
+                    <div class="number"> Number: <span class="span">(083) 552.4484</span></div>
+                    <div class="fax"> Fax No.: <span class="span">(083) 552.4446</span></div>
                 </div>
             </div>
         </div>
@@ -34,7 +35,7 @@
             </div>
         </div>
         <div class="billing-to">
-            <div class="title"> Billed to </div>
+            <div class="title"> Prepared by: </div>
             <div class="billed-sec">
                 <div class="name">
                     Account name
@@ -44,8 +45,8 @@
             </div>
     
             <div class="billed-sec">
-                <div class="sub-title"> shipping Address </div>
-                <div class="ship-add"> Address to </div>
+                <div class="sub-title"> Role </div>
+                <div class="ship-add"> Admin </div>
             </div>
         </div>
         <div class="table">
@@ -115,6 +116,14 @@
             </tr>
 
         </table>
+    </div>
+    <div class="bottom-section">
+        <div class="status-content">
+            <h4> Payment Status </h4>
+            <p class="status free"></p>
+            <p> Payment Method: <span> SSL Commerz</span></p>
+            <p class="tnx">  </p>
+        </div>
     </div>
     </div>
 
