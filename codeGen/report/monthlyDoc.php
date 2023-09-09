ac8295907e08b938ae927390a45636e04e54b654
Cris Mateo Uriarte <39321306+criskhun@users.noreply.github.com>
2023-09-09 12:21:59 +0800

    Update monthlyDoc.php

--- codeGen/report/monthlyDoc.php
+++ codeGen/report/monthlyDoc.php
@@ -78,7 +78,86 @@ $sqlMonthly = "SELECT * FROM documentCG_tbl ORDER BY id DESC";
 
 
 <!-- Rest of your HTML code -->
+<script>
+    function exportToExcel() {
+    // Create a new workbook
+    var workbook = XLSX.utils.book_new();
 
+    // Get the table element
+    var table = document.getElementById("Monthlytlb");
+
+    // Get the table headers
+    var headers = [];
+    for (var i = 0; i < table.rows[0].cells.length; i++) {
+        headers.push(table.rows[0].cells[i].textContent);
+    }
+
+    // Create an array to store the table data
+    var tableData = [headers];
+
+    // Iterate through the table rows and collect data
+    for (var i = 1; i < table.rows.length; i++) {
+        var rowData = [];
+        for (var j = 0; j < table.rows[i].cells.length; j++) {
+            rowData.push(table.rows[i].cells[j].textContent);
+        }
+        tableData.push(rowData);
+    }
+
+    // Add the custom title as the first row
+    tableData.unshift(["Monthly Transaction"]);
+
+    // Create a worksheet and add data to it
+    var worksheet = XLSX.utils.aoa_to_sheet(tableData);
+
+    // Style the custom title (make it bold)
+    worksheet["A1"].s = { font: { bold: true } };
+
+    // Add the worksheet to the workbook
+    var currentYear = currentDate.getFullYear();
+    var currentMonth = currentDate.getMonth() + 1;
+    var formattedDate = `${currentYear}-${currentMonth}`;
+    XLSX.utils.book_append_sheet(workbook, worksheet, formattedDate);
+
+    // Save the workbook as an XLSX file
+    XLSX.writeFile(workbook, "MonthProcessCG.xlsx");
+}
+
+    // Add a click event listener to the export button
+    document.getElementById("exportButtonMonth").addEventListener("click", exportToExcel);
+
+    // Get a reference to the second button element
+var printButtonMonth = document.getElementById("printButtonMonth");
+
+// Add a click event listener to the second button
+printButtonMonth.addEventListener("click", function () {
+    // Get the filtered table data for the second table (modify as needed)
+    var filteredTableData = getFilteredTableDataForMonth();
+
+    // Encode the filtered data as a query parameter
+    var queryParams = encodeURIComponent(JSON.stringify(filteredTableData));
+
+    // Redirect to the other page with the query parameter
+    window.location.href = "report/printDesign.php?tableData=" + queryParams;
+});
+
+// Function to get the filtered table data for the second table (modify as needed)
+function getFilteredTableDataForMonth() {
+    var filteredData = [];
+
+    // Modify this selector to target the second table's rows
+    $("#tableBodyMonthly tr:visible").each(function () {
+        var rowData = [];
+        $(this).find("td").each(function () {
+            rowData.push($(this).text().trim());
+        });
+        filteredData.push(rowData);
+    });
+
+    return filteredData;
+}
+
+</script>
 <script>
      $(document).ready(function() {
         function updateRowCountMonthly() {
