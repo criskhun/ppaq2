597c0d7bad902e1ec6a4ef8dab6d3eda84548a52
Cris Mateo Uriarte <39321306+criskhun@users.noreply.github.com>
2023-09-09 12:20:06 +0800

    Update monthlyDoc.php

--- codeGen/report/monthlyDoc.php
+++ codeGen/report/monthlyDoc.php
@@ -78,149 +78,114 @@ $sqlMonthly = "SELECT * FROM documentCG_tbl ORDER BY id DESC";
 
 
 <!-- Rest of your HTML code -->
+
 <script>
-    function exportToExcel() {
-    // Create a new workbook
-    var workbook = XLSX.utils.book_new();
-
-    // Get the table element
-    var table = document.getElementById("Monthlytlb");
-
-    // Get the table headers
-    var headers = [];
-    for (var i = 0; i < table.rows[0].cells.length; i++) {
-        headers.push(table.rows[0].cells[i].textContent);
-    }
-
-    // Create an array to store the table data
-    var tableData = [headers];
-
-    // Iterate through the table rows and collect data
-    for (var i = 1; i < table.rows.length; i++) {
-        var rowData = [];
-        for (var j = 0; j < table.rows[i].cells.length; j++) {
-            rowData.push(table.rows[i].cells[j].textContent);
-        }
-        tableData.push(rowData);
-    }
-
-    // Add the custom title as the first row
-    tableData.unshift(["Monthly Transaction"]);
-
-    // Create a worksheet and add data to it
-    var worksheet = XLSX.utils.aoa_to_sheet(tableData);
-
-    // Style the custom title (make it bold)
-    worksheet["A1"].s = { font: { bold: true } };
-
-    // Add the worksheet to the workbook
-    var currentYear = currentDate.getFullYear();
-    var currentMonth = currentDate.getMonth() + 1;
-    var formattedDate = `${currentYear}-${currentMonth}`;
-    XLSX.utils.book_append_sheet(workbook, worksheet, formattedDate);
-
-    // Save the workbook as an XLSX file
-    XLSX.writeFile(workbook, "MonthProcessCG.xlsx");
-}
-
-    // Add a click event listener to the export button
-    document.getElementById("exportButtonMonth").addEventListener("click", exportToExcel);
-
-    // Get a reference to the second button element
-var printButtonMonth = document.getElementById("printButtonMonth");
-
-// Add a click event listener to the second button
-printButtonMonth.addEventListener("click", function () {
-    // Get the filtered table data for the second table (modify as needed)
-    var filteredTableData = getFilteredTableDataForMonth();
-
-    // Encode the filtered data as a query parameter
-    var queryParams = encodeURIComponent(JSON.stringify(filteredTableData));
-
-    // Redirect to the other page with the query parameter
-    window.location.href = "report/printDesign.php?tableData=" + queryParams;
-});
-
-// Function to get the filtered table data for the second table (modify as needed)
-function getFilteredTableDataForMonth() {
-    var filteredData = [];
-
-    // Modify this selector to target the second table's rows
-    $("#tableBodyMonthly tr:visible").each(function () {
-        var rowData = [];
-        $(this).find("td").each(function () {
-            rowData.push($(this).text().trim());
-        });
-        filteredData.push(rowData);
-    });
-
-    return filteredData;
-}
-
-</script>
-<script>
-     $(document).ready(function() {
-        function updateRowCountMonthly() {
-            const visibleRowCount = $("#tableBodyMonthly tr:visible").length;
-            $("#rowCountMonthly").text(`Total rows: ${visibleRowCount}`);
-        }
-
-        function filterTableRowsByMonth(selectedMonth) {
-            $("#tableBodyMonthly tr").each(function () {
-                const rowDate = moment($(this).find("td:eq(6)").text()); // Assuming date is in the 6th column
-
-                if (rowDate.isSame(selectedMonth, 'month')) {
-                    $(this).show();
-                } else {
-                    $(this).hide();
-                }
-            });
-
-            updateRowCountMonthly();
-        }
-
-        // Initial filter by current month
-        const currentDate = moment();
-        const initialMonth = currentDate.format('YYYY-MM');
-        filterTableRowsByMonth(currentDate);
-
-        // Handle changes in the month picker
-        $("#monthPicker").on("change", function() {
-            const selectedMonth = moment($(this).val(), 'YYYY-MM');
-            filterTableRowsByMonth(selectedMonth);
-        });
-
-        function filterTableRows(searchValue) {
-            searchValue = searchValue.toLowerCase();
-
-            $("#tableBodyMonthly tr").each(function () {
-                const rowText = $(this).text().toLowerCase();
-                const rowDate = moment($(this).find("td:eq(6)").text()); // Assuming date is in the 6th column
-
-                if (rowDate.isSame(selectedMonth, 'month') && rowText.includes(searchValue)) {
-                    $(this).show();
-                } else {
-                    $(this).hide();
-                }
-            });
-
-            updateRowCountMonthly();
-        }
-
-        $("#searchInputMonthly").on("input", function() {
-            const searchValue = $(this).val();
-            filterTableRows(searchValue);
-        });
-
-        // When clearing the filter input, show only the current month data
-        $("#searchInputMonthly").on("keyup", function() {
-            if ($(this).val() === "") {
-                filterTableRowsByMonth(selectedMonth);
+        var selectedMonth;
+        
+        $(document).ready(function() {
+            function updateRowCountMonthly() {
+                const visibleRowCount = $("#tableBodyMonthly tr:visible").length;
+                $("#rowCountMonthly").text(`Total rows: ${visibleRowCount}`);
             }
+        
+            function filterTableRowsByMonth(selectedMonth) {
+                $("#tableBodyMonthly tr").each(function () {
+                    const rowDate = moment($(this).find("td:eq(6)").text(), 'YYYY-MM-DD'); // Assuming date is in the 6th column
+        
+                    if (rowDate.isSame(selectedMonth, 'month')) {
+                        $(this).show();
+                    } else {
+                        $(this).hide();
+                    }
+                });
+        
+                updateRowCountMonthly();
+            }
+        
+            // Handle changes in the month picker
+            $("#monthPicker").on("change", function() {
+                selectedMonth = moment($(this).val(), 'YYYY-MM');
+                filterTableRowsByMonth(selectedMonth);
+            });
+        
+            function filterTableRows(searchValue) {
+                searchValue = searchValue.toLowerCase();
+        
+                $("#tableBodyMonthly tr").each(function () {
+                    const rowText = $(this).text().toLowerCase();
+                    const rowDate = moment($(this).find("td:eq(6)").text(), 'YYYY-MM-DD'); // Assuming date is in the 6th column
+        
+                    if (rowDate.isSame(selectedMonth, 'month') && rowText.includes(searchValue)) {
+                        $(this).show();
+                    } else {
+                        $(this).hide();
+                    }
+                });
+        
+                updateRowCountMonthly();
+            }
+        
+            $("#searchInputMonthly").on("input", function() {
+                const searchValue = $(this).val();
+                filterTableRows(searchValue);
+            });
+        
+            // When clearing the filter input, show only the current month data
+            $("#searchInputMonthly").on("keyup", function() {
+                if ($(this).val() === "") {
+                    filterTableRowsByMonth(selectedMonth);
+                }
+            });
+        
+            // Function to export table data to Excel for the monthly report
+            function exportToExcel() {
+                // Create a new workbook
+                var workbook = XLSX.utils.book_new();
+        
+                // Get the table element
+                var table = document.getElementById("MonthlyTable");
+        
+                // Get the table headers
+                var headers = [];
+                for (var i = 0; i < table.rows[0].cells.length; i++) {
+                    headers.push(table.rows[0].cells[i].textContent);
+                }
+        
+                // Create an array to store the table data
+                var tableData = [headers];
+        
+                // Iterate through the table rows and collect data for the selected month
+                $("#tableBodyMonthly tr:visible").each(function () {
+                    var rowDate = moment($(this).find("td:eq(6)").text(), 'YYYY-MM-DD'); // Assuming date is in the 6th column
+                    if (rowDate.isSame(selectedMonth, 'month')) {
+                        var rowData = [];
+                        for (var j = 0; j < $(this).find("td").length; j++) {
+                            rowData.push($(this).find("td:eq(" + j + ")").text().trim());
+                        }
+                        tableData.push(rowData);
+                    }
+                });
+        
+                // Add the custom title as the first row
+                tableData.unshift(["Monthly Transaction"]);
+        
+                // Create a worksheet and add data to it
+                var worksheet = XLSX.utils.aoa_to_sheet(tableData);
+        
+                // Style the custom title (make it bold)
+                worksheet["A1"].s = { font: { bold: true } };
+        
+                // Format the date as YYYY-MM
+                var formattedDate = selectedMonth.format('YYYY-MM');
+        
+                // Add the worksheet to the workbook
+                XLSX.utils.book_append_sheet(workbook, worksheet, formattedDate);
+        
+                // Save the workbook as an XLSX file
+                XLSX.writeFile(workbook, "MonthProcessCG.xlsx");
+            }
+        
+            // Add a click event listener to the export button
+            document.getElementById("exportButtonMonth").addEventListener("click", exportToExcel);
         });
-
-        // Initial row count display
-        updateRowCountMonthly();
-    });
-
-</script>
+    </script>
