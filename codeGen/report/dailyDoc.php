b979d9f7cbde3214ec6bbfac6ecaa1d59e42e2d8
Cris Mateo Uriarte <39321306+criskhun@users.noreply.github.com>
2023-09-11 09:00:28 +0800

    Update dailyDoc.php

--- codeGen/report/dailyDoc.php
+++ codeGen/report/dailyDoc.php
@@ -13,7 +13,7 @@ $sqldaily = "SELECT * FROM documentCG_tbl ORDER BY id DESC";
 
     <div>
         <button class="btn btn-success" id="printButtonDay"><i class="fa-solid fa-print"></i> Print</button>
-        <button class="btn btn-success" id="exportButton"><i class="fa-solid fa-file-export"></i> Export</button>
+        <button class="btn btn-success" id="exportButtonDay"><i class="fa-solid fa-file-export"></i> Export</button>
     </div>
 </div>
 <br>
@@ -24,7 +24,7 @@ $sqldaily = "SELECT * FROM documentCG_tbl ORDER BY id DESC";
 
         </div>
     </div>
-                <table class="table table-striped table-borderless table-hover">
+                <table id="daytlb" class="table table-striped table-borderless table-hover">
                     <thead class="print-header">
                         <tr>
                             <th>#</th>
@@ -64,6 +64,71 @@ $sqldaily = "SELECT * FROM documentCG_tbl ORDER BY id DESC";
                 </table>
 
 <script>
+    document.getElementById("exportButtonDay").addEventListener("click", exportToExcel);
+
+function exportToExcel() {
+    // Create a new workbook
+    var workbook = XLSX.utils.book_new();
+
+    // Get the table element
+    var table = document.getElementById("daytlb");
+
+    // Get the table headers
+    var headers = [];
+    for (var i = 0; i < table.rows[0].cells.length; i++) {
+        headers.push(table.rows[0].cells[i].textContent);
+    }
+
+    // Create an array to store the table data
+    var tableData = [headers];
+
+    // Iterate through the table rows and collect data
+    for (var i = 1; i < table.rows.length; i++) {
+        var rowData = [];
+        for (var j = 0; j < table.rows[i].cells.length; j++) {
+            rowData.push(table.rows[i].cells[j].textContent);
+        }
+        tableData.push(rowData);
+    }
+
+    var currentDate = new Date();
+var currentYear = currentDate.getFullYear();
+var currentMonth = currentDate.toLocaleString('default', { month: 'long' }); // Get the full month name
+var currentDayOfWeek = getDayOfWeek(currentDate); // Function to get the day of the week
+
+// Add the custom title as the first row
+tableData.unshift([currentDayOfWeek + " (" + currentDate.getDate() + ")-" + currentMonth + "-" + currentYear]);
+
+// Function to get the day of the week
+function getDayOfWeek(date) {
+    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    return daysOfWeek[date.getDay()];
+}
+
+// Function to get the week number
+function getWeekNumber(date) {
+    date = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()));
+    date.setUTCDate(date.getUTCDate() + 4 - (date.getUTCDay() || 7));
+    var yearStart = new Date(Date.UTC(date.getUTCFullYear(), 0, 1));
+    var weekNumber = Math.ceil((((date - yearStart) / 86400000) + 1) / 7);
+    return weekNumber;
+}
+
+    // Create a worksheet and add data to it
+    var worksheet = XLSX.utils.aoa_to_sheet(tableData);
+
+    // Style the custom title (make it bold)
+    worksheet["A1"].s = { font: { bold: true } };
+
+    // Add the worksheet to the workbook
+    XLSX.utils.book_append_sheet(workbook, worksheet, currentDayOfWeek + " (" + currentDate.getDate() + ")-" + "_" + currentMonth + "-" + currentYear);
+
+    // Save the workbook as an XLSX file
+    XLSX.writeFile(workbook, "DailyProcessCG.xlsx");
+}
+
+document.getElementById("exportButtonDay").addEventListener("click", exportToExcel);
+
 // Get a reference to the "Print" button element
 var printButton = document.getElementById("printButtonDay");
 
